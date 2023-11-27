1dfecbe03ceb3c5024b8aa78d3cb467ec0a2c41f
YsN23 <yy2369964@gmail.com>
2023-11-27 19:49:45 +0100

    Done Task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,21 +8,15 @@
 
 int check_cycle(listint_t *list)
 {
-	listint_t *slow, *fast;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	slow = list;
-	fast = list;
-
-	while (slow != NULL && fast != NULL)
+	while (fast != NULL && slow != NULL && fast->next != NULL)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
-
-		if (fast == slow)
-		{
+		if (slow == fast)
 			return (1);
-		}
 	}
-
 	return (0);
 }
