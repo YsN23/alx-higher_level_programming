0453cabf9484ed04762ee01616f985c3a284c92a
YsN23 <yy2369964@gmail.com>
2023-11-28 19:19:32 +0100

    Done

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,18 +1,4 @@
 #!/usr/bin/python3
-
-"""def fizzbuzz():
-    for i in range(1, 101):
-        if (i % 3 == 0) and (i % 5 == 0):
-            print("FizzBuzz")
-        elif i % 3 == 0:
-            print("Fizz")
-        elif i % 5 == 0:
-            print("Buzz")
-        elif i == 100:
-            print("")
-        else:
-            print("{}".format(i), end=" ")"""
-
 def fizzbuzz():
     for i in range(1, 101):
         if (i % 3 == 0) and (i % 5 == 0):
