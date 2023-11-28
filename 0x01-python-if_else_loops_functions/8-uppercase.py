bf906ab75f902b650eefe3b290d4368438233dd2
YsN23 <yy2369964@gmail.com>
2023-11-28 11:27:22 +0100

    Done

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+def uppercase(str):
+    upper_string = ""
+
+    for char in str:
+        ascii_num = ord(char)
+
+        if 97 <= ascii_num <= 122:
+            new_ascii = ascii_num - 32
+            upper_char = chr(new_ascii)
+            upper_string += upper_char
+
+        else:
+            upper_string += char
+
+    return upper_string
