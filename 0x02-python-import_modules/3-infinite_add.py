6107f26033678e0cb5b4e7caf0458bf9e1267874
YsN23 <yy2369964@gmail.com>
2023-11-30 17:13:29 +0100

    Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+
+    from sys import argv
+
+    lenght = len(argv) - 1
+    result = 0
+
+    if lenght == 0:
+        print("0")
+
+    else:
+        for i in argv[1:]:
+            if not i.isdigit():
+                continue
+
+            result += int(i)
+        print("{}".format(result))
