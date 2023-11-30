fdb3be41058f8e48332510108aaeee32eab92b34
YsN23 <yy2369964@gmail.com>
2023-11-30 16:25:59 +0100

    Done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from sys import argv
+
+    if not argv or len(argv) == 1:
+        print(f"0 arguments.")
+
+    else:
+        print("{} arguments:".format(len(argv) - 1))
+        for i, args in enumerate(argv[1:]):
+
+            print("{}: {}".format(i + 1, args))
