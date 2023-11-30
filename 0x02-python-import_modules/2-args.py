9880babe6bd0a09c3c0b5bc42b229501e1614699
YsN23 <yy2369964@gmail.com>
2023-11-30 16:50:18 +0100

    L3asab almlawi

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,13 @@
 if __name__ == "__main__":
     from sys import argv
 
-    if not argv or len(argv) == 1:
-        print("{} arguments.".format(len(argv) - 1))
-
+    if len(argv) == 1:
+        print("0 arguments.")
+    elif len(argv) - 1 == 1:
+        print("{} arguments:".format(len(argv) - 1))
+        print("{}: {}".format(len(argv) - 1, argv[1]))
     else:
         print("{} arguments:".format(len(argv) - 1))
 
-    for i, args in enumerate(argv[1:]):
-        print("{}: {}".format(i + 1, args))
+        for i, args in enumerate(argv[1:]):
+            print("{}: {}".format(i + 1, args))
