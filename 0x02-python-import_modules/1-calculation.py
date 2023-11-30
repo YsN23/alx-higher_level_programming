49119186df59fdea9fa5076ca5f25dad1f680de1
YsN23 <yy2369964@gmail.com>
2023-11-30 12:10:06 +0100

    Done

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+    fu1 = add(a, b)
+    fu2 = sub(a, b)
+    fu3 = mul(a, b)
+    fu4 = div(a, b)
+
+    print("{} + {} = {}".format(a, b, fu1))
+    print("{} - {} = {}".format(a, b, fu2))
+    print("{} * {} = {}".format(a, b, fu3))
+    print("{} / {} = {}".format(a, b, fu4))
