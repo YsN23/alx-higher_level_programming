ad35e840ffef7eb58a54e329e53e47dcfa5490aa
YsN23 <yy2369964@gmail.com>
2023-12-01 20:17:55 +0100

    Done

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    leght = len(my_list)
+
+    new_list = []
+
+    new_list = my_list
+
+    if idx < 0 or idx >= leght:
+        return my_list
+
+    for i in range(leght):
+        if i == idx:
+            new_list[i] = new_list[i].replace(element)
+        else:
+            new_list[i] = new_list[i]
+
+    return new_list
