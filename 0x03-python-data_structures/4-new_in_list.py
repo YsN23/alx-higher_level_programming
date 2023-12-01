fae96078542319728d0298bd67459677680c4e09
YsN23 <yy2369964@gmail.com>
2023-12-01 20:34:31 +0100

    Done

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    new_list = []
+
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    for i in my_list:
+        new_list.append(i)
+
+    for i in range(len(new_list)):
+        if i == idx:
+            new_list[i] = element
+        else:
+            new_list[i] = new_list[i]
+
+    return new_list
