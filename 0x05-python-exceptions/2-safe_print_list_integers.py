1e215996fdcb8fa8521bf9fe4a7db7883cb81b65
YsN23 <yy2369964@gmail.com>
2023-12-18 20:21:51 +0100

    Done

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,7 +11,7 @@ def safe_print_list_integers(my_list=[], x=0):
                 count += 1
             else:
                 continue
-    except (IndexError, ValueError):
+    except IndexError and ValueError:
         pass
 
     print()
