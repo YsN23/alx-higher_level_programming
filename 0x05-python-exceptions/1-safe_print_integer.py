d4458dd5ae3d465686d9d7304ca92594baad754b
YsN23 <yy2369964@gmail.com>
2023-12-18 19:30:21 +0100

    Done

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def safe_print_integer(value):
+
+    try:
+        if isinstance(value, int):
+            print("{:d}".format(value))
+            return True
+        else:
+            return False
+    except ValueError:
+        pass
