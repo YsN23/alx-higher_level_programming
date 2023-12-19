90818a83a3589b1777b5bd3c056eeb031676fdce
YsN23 <yy2369964@gmail.com>
2023-12-19 20:50:32 +0100

    Done

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,1 +1,40 @@
 #!/usr/bin/python3
+"""Square Class Implementation to represent a square"""
+
+
+class Square:
+    """Constructor method to initialize a Square object."""
+    def __init__(self, size=0):
+        """Initializing Method"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Size Getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Property for Size Setting"""
+        self.__size = value
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+
+    def area(self):
+        """Calculate and return the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Printing The Square according to area"""
+        if self.__size == 0:
+            print()
+
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
