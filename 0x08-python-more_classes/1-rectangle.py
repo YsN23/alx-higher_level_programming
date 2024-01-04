fc2b876b63c3a95e00dde7f1999ccc29a4f482e7
YsN23 <yy2369964@gmail.com>
2024-01-04 13:27:54 +0100

    Done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,44 +1,38 @@
 #!/usr/bin/python3
-"""Rectangle Class Implementation"""
+"""Class Rectangle"""
 
 
 class Rectangle:
-    """Real Implementation of Rectangle Class"""
+    """Methods in class Rectangles"""
     def __init__(self, width=0, height=0):
         """Constructor"""
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
-        """Getter"""
+        """Getter method for width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter"""
-
+        """Setter method"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-
         if value < 0:
             raise ValueError("width must be >= 0")
-
         self.__width = value
 
     @property
     def height(self):
-        """Getter"""
+        """Getter method for height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter"""
-
+        """Setter method for height"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
-
         if value < 0:
             raise ValueError("height must be >= 0")
-
         self.__height = value
