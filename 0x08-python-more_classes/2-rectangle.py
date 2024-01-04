4d3b562d1de58a8bc38a4b99f3fa7c629a43e25a
YsN23 <yy2369964@gmail.com>
2024-01-04 13:35:32 +0100

    Done

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,1 +1,57 @@
 #!/usr/bin/python3
+"""Rectangle Class Implementation"""
+
+
+class Rectangle:
+    """Real Implementation of Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """Constructor"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter"""
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter"""
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
+
+        self.__height = value
+
+    def area(self):
+        """Area of The Rectangle"""
+        area = self.__height * self.__width
+        return area
+
+    def perimeter(self):
+        if self.__height == 0 or self.__width == 0:
+            perimeter = 0
+        else:
+            perimeter = (self.__height + self.__width) * 2
+
+        return perimeter
