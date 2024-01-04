675fade9adb0146f3f1e41cc855daee9da012834
YsN23 <yy2369964@gmail.com>
2024-01-04 20:13:20 +0100

    Done

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -66,16 +66,3 @@ class Rectangle:
             if i != self.__height - 1:
                 square += "\n"
         return square
-
-my_rectangle = Rectangle(2, 4)
-print("Area: {} - Perimeter: {}".format(my_rectangle.area(), my_rectangle.perimeter()))
-
-print(str(my_rectangle))
-print(repr(my_rectangle))
-
-print("--")
-
-my_rectangle.width = 10
-my_rectangle.height = 3
-print(my_rectangle)
-print(repr(my_rectangle))
