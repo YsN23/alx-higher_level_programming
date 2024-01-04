83d8593cd3bacc8f833b4d377fd565d5d3eb5178
YsN23 <yy2369964@gmail.com>
2024-01-04 13:11:03 +0100

    Done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,1 +1,42 @@
 #!/usr/bin/python3
+"""Rectangle Class Implementation"""
+
+
+class Rectangle:
+    """Real Implementation of Rectangle Class"""
+    def __init__(self, width=0, height=0):
+        """Constructor"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter"""
+        self.__width = value
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+
+        if value < 0:
+            raise ValueError("width must be >= 0")
+
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter"""
+        self.__height = value
+
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+
+        if value < 0:
+            raise ValueError("height must be >= 0")
