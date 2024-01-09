9eabd28ec2968339bd999f77b951832cfb75e683
YsN23 <yy2369964@gmail.com>
2024-01-09 13:32:19 +0100

    Done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Appending Mode"""
+
+
+def append_write(filename="", text=""):
+    """Append To The EOF and return chars (text)"""
+
+    with open(filename, 'a', encoding="utf-8") as f:
+
+        f.write("\n{}".format(text))
+
+        return len(text)
