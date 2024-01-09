09bbc44890ceedf8bd1628c64818ee63846fc014
YsN23 <yy2369964@gmail.com>
2024-01-09 16:13:54 +0100

    Done

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Working With JSON in Python"""
+import json
+
+
+def from_json_string(my_str):
+    """Deserialization """
+
+    py_obj = json.loads(my_str)
+
+    return py_obj
