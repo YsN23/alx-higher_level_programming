445f0260a9f852a803caf1d32098e357575d66ad
YsN23 <yy2369964@gmail.com>
2024-01-09 16:27:45 +0100

    Done

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -6,7 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Serialization and Writing To JSON file"""
 
-    json_format_string = json.dumps(my_obj)
-
     with open(filename, 'w', encoding='utf-8') as f:
-        f.write(json_format_string)
+        json.dump(my_obj, f)
