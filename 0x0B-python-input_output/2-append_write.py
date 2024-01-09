1baf90368ed5e23ef83e59b87e678a78999a0823
YsN23 <yy2369964@gmail.com>
2024-01-09 13:33:24 +0100

    Done

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,6 +7,6 @@ def append_write(filename="", text=""):
 
     with open(filename, 'a', encoding="utf-8") as f:
 
-        f.write("\n{}".format(text))
+        f.write(text)
 
         return len(text)
