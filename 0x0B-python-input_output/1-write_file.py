a6f5874604e15f1ae0566bf6d24d6889efbf2ec7
YsN23 <yy2369964@gmail.com>
2024-01-09 13:23:17 +0100

    Done

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,8 +3,10 @@
 
 
 def write_file(filename="", text=""):
-    """Function That Writes text into a file and Returns the lenght of the chars"""
+    """Function That Writes text into a file and Returns num of chars"""
 
     with open(filename, 'w', encoding='utf-8') as f:
 
-        return f.write(text)
+        f.write(text)
+
+        return len(text)
