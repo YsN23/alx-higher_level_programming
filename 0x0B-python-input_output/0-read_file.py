a655d9b9b8d89f8e088f6a3631de1581a0824e35
YsN23 <yy2369964@gmail.com>
2024-01-09 13:03:56 +0100

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Function That Reads a File (UTF-8)"""
+
+
+def read_file(filename=""):
+
+    with open(filename, 'r', encoding='utf-8') as f:
+
+        content = f.read()
+
+        print(content)
