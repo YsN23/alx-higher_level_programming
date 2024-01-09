ffcdf952fbb5ccebc1d361e18caf4c6124ca063d
YsN23 <yy2369964@gmail.com>
2024-01-09 16:54:34 +0100

    Done

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Working With JSON in Python"""
+import json
+
+
+def load_from_json_file(filename):
+    """Reading and Deserialization"""
+
+    with open(filename, 'r', encoding='utf-8') as f:
+
+        return json.load(f)
