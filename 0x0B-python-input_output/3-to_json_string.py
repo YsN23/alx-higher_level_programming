88c8c3d2fdd6eec024ae951309cd12ad13357e6c
YsN23 <yy2369964@gmail.com>
2024-01-09 16:10:28 +0100

    Done

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Working With JSON in Python"""
+
+import json
+
+def to_json_string(my_obj):
+    """Serialization"""
+
+    json_string = json.dumps(my_obj)
+
+    return json_string
