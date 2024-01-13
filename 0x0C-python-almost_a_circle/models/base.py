fcfb4091fc8373bbb6aa8eda6031cd9302d25421
YsN23 <yy2369964@gmail.com>
2024-01-14 00:49:15 +0100

    Done

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""Base Class"""
+
+
+class Base:
+    """Implementation of Base Class"""
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
